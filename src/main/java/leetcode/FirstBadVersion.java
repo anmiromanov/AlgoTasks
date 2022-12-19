ca7dcd9c308847c7e19897213acf454ad6943c4b
AnMiRomanov <am-romanov@mail.ru>
2022-12-19 22:53:43 +0500

    Решение задачи Leetcode: FirstBadVersion

---
+++ src/main/java/leetcode/FirstBadVersion.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+public class FirstBadVersion {
+    public int firstBadVersion(int n) {
+        int minVersion = 1;
+        int maxVersion = n;
+        while (minVersion < maxVersion) {
+            int mid = minVersion + (maxVersion - minVersion) / 2;
+            if (isBadVersion(mid)) {
+                maxVersion = mid;
+            } else {
+                minVersion = mid + 1;
+            }
+        }
+        return minVersion;
+    }
+
+    public boolean isBadVersion(int n){
+        return true;
+    }
+}
