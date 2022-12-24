d7f530c3c54a0a9f657b40791d440280607f81a1
AnMiRomanov <am-romanov@mail.ru>
2022-12-24 16:23:43 +0500

    Решение задачи Leetcode: 160. Intersection of Two Linked Lists

---
+++ src/main/java/leetcode/twopointrs/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,38 @@
+package leetcode.twopointrs;
+/*
+160. Intersection of Two Linked Lists
+https://leetcode.com/problems/intersection-of-two-linked-lists/
+*/
+public class IntersectionOfTwoLinkedLists {
+    class ListNode{
+        int val;
+        ListNode next;
+        ListNode(int x){
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode aPoint = headA;
+        ListNode bPoint = headB;
+
+        while (aPoint != bPoint){
+            if (aPoint == null){
+                aPoint = headB;
+            } else {
+                aPoint = aPoint.next;
+            }
+
+            if (bPoint == null){
+                bPoint = headA;
+            } else {
+                bPoint = bPoint.next;
+            }
+        }
+        return aPoint;
+    }
+}
